603c42cf404a2d4483c0871e3145ec225bb4b038
chxlvwh <807051073@qq.com>
2018-05-27 20:30:55 +0800

    组件

--- src/app.js
+++ src/app.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom';
 
 require('./common/style/main.less');
 require('./common/style/reset.css');
-ReactDOM.render(
-	<div className="comments">
-		<header>Comments</header>
-		<div className="content">
+
+const elyse = require('./common/img/elyse.png');
+const molly = require('./common/img/molly.png');
+const matthew = require('./common/img/matthew.png');
+
+function isInner(i) {
+	if (i!=undefined) {
+		if (!i.length) {
+			return null;
+		} else {
+			// let _i = i.slice(1)
+			let name = Object.values(i[0])[0]
+			return (
+				<Comment name={name}/>
+			)
+			// isInner(_i)
+		}
+	}
+}
+class Comment extends Component {
+	render() {
+		return (
 			<div className="item clear">
 				<div className="img">
-					<img src={require('./common/img/elyse.png')}/>
+					<img src={this.props.name}/>
 				</div>
 				<div className="commentInfo">
 					<h4 className="name">Matt</h4>
@@ -18,49 +36,34 @@ ReactDOM.render(
 					</p>
 					<p className="reply">回复</p>
 				</div>
+				{isInner(this.props.inner)}
 			</div>
-			<div className="item clear">
-				<div className="img">
-					<img src={require('./common/img/molly.png')}/>
-				</div>
-				<div className="commentInfo">
-					<h4 className="name">Matt</h4>
-					<p className="commentContent">
-						what?
-					</p>
-					<p className="reply">回复</p>
-				</div>
-				<div className="item clear">
-					<div className="img">
-						<img src={require('./common/img/matthew.png')}/>
-					</div>
-					<div className="commentInfo">
-						<h4 className="name">Matt</h4>
-						<p className="commentContent">
-							it is~~~~~
-						</p>
-						<p className="reply">回复</p>
-					</div>
-				</div>
-			</div>
+		)
+	}
+}
 
-			<div className="item clear">
-				<div className="img">
-					<img src={require('./common/img/molly.png')}/>
-				</div>
-				<div className="commentInfo">
-					<h4 className="name">Matt</h4>
-					<p className="commentContent">
-						congratulations ! ! !
-					</p>
-					<p className="reply">回复</p>
-				</div>
-			</div>
+class CommentDate extends Component {
+	render() {
+		return (
+			<div className="commentDate">{this.props.day}</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<div className="comments">
+		<header>Comments</header>
+		<CommentDate day="今天"></CommentDate>
+		<div className="content">
+			<Comment name={elyse} inner={[{molly}]}/>
+			<Comment name={elyse} inner={[{matthew}]}/>
+			<Comment name={molly}/>
+			<Comment name={matthew}/>
 		</div>
 	</div>,
 	document.getElementById('root')
 );
 
-if(module.hot){ 
-    module.hot.accept()
-}
+// if(module.hot){ 
+//     module.hot.accept()
+// }
