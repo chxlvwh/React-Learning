aed73355c9e158a9cd402ae9138a73c894f9c3cf
caohx <caohx@ynt.ai>
2018-06-01 18:37:58 +0800

    点击显示回复框

--- src/app.js
+++ src/app.js
@@ -8,20 +8,45 @@ const elyse = require('./common/img/elyse.png');
 const molly = require('./common/img/molly.png');
 const matthew = require('./common/img/matthew.png');
 
-function isInner(i) {
-	if (i!=undefined) {
-		if (!i.length) {
-			return null;
+class Comment extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { showTextarea: false }
+		this.changeShowTextarea = this.changeShowTextarea.bind(this)
+	}
+
+	changeShowTextarea() {
+		if (this.state.showTextarea) {
+			this.setState({ showTextarea: false })
 		} else {
-			let name = Object.values(i[0])[0]
-			i.shift()
-			return (
-				<Comment name={name} inner={i}/>
-			)
+			this.setState({ showTextarea: true })
 		}
 	}
-}
-class Comment extends Component {
+
+	isInner(i) {
+		if (i!=undefined) {
+			if (!i.length) {
+				return null;
+			} else {
+				let name = Object.values(i[0])[0]
+				i.shift()
+				return (
+					<Comment name={name} inner={i}/>
+				)
+			}
+		}
+	}
+
+	showReplyWrapper(arg) {
+		if (arg) {
+			return (
+				<ReplyWrapper />
+			)
+		} else {
+			return null
+		}
+	}
+
 	render() {
 		return (
 			<div className="item clear">
@@ -33,9 +58,34 @@ class Comment extends Component {
 					<p className="commentContent">
 						How artistic!
 					</p>
-					<p className="reply">回复</p>
+					<p className="reply" onClick={this.changeShowTextarea}>回复</p>
+					{this.showReplyWrapper(this.state.showTextarea)}
 				</div>
-				{isInner(this.props.inner)}
+				{this.isInner(this.props.inner)}
+			</div>
+		)
+	}
+}
+
+class ReplyWrapper extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { replyContent: '' }
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(e) {
+		this.setState({ replyContent: e.target.value })
+	}
+
+	render() {
+		return (
+			<div>
+				<textarea cols="30" rows="10"
+					style={{border: '1px solid #eee',marginBottom: '15px'}}
+					value={this.state.replyContent}
+				 	onChange={this.handleChange}
+				/>
 			</div>
 		)
 	}
